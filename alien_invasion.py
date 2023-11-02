1540d90e7aff5fdd41b2d69e0c81d7811d8851a7
ericdgy <ericdgy97@gmail.com>
2023-11-02 17:44:17 +0800

    Python复健

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,7 +92,20 @@ class AlienInvasion:
         self.aliens.add(alien)
 
     def _update_aliens(self):
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
